a1b6c5d4552cf1d20c88e8263980212949d0f271
Holker Dev <nick.penkovskiy@gmail.com>
2021-12-08 18:41:23 +0100

    add tests

--- src/test/kotlin/eu/holker/classic/controllers/RecordControllerTest.kt
+++ src/test/kotlin/eu/holker/classic/controllers/RecordControllerTest.kt
@@ -18,14 +18,17 @@ class RecordControllerTest {
         recordService.stub { on { findAllRecords() }.doReturn(listOf()) }
         val res = controller.getAll()
         res.statusCode shouldBeEqualTo HttpStatus.OK
+        res.body shouldBeEqualTo listOf<RecordDto>()
     }
 
     @Test
     fun `record controller getById should return 200 when record is found`() {
         val recordId = 12
-        recordService.stub { on { findRecordById(recordId) }.doReturn(Result.success(RecordDto(12, "dawdaw"))) }
+        val dto = RecordDto(12, "dawdawd")
+        recordService.stub { on { findRecordById(recordId) }.doReturn(Result.success(dto)) }
         val res = controller.getById(recordId = recordId.toString())
         res.statusCode shouldBeEqualTo HttpStatus.OK
+        res.body shouldBeEqualTo dto
     }
 
     @Test
@@ -34,5 +37,6 @@ class RecordControllerTest {
         recordService.stub { on { findRecordById(recordId) }.doReturn(Result.failure(Exception("Error message test"))) }
         val res = controller.getById(recordId = recordId.toString())
         res.statusCode shouldBeEqualTo HttpStatus.BAD_REQUEST
+        res.body shouldBeEqualTo "Error message test"
     }
 }
