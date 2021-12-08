d580d36da7d81e6d659af17975af91dc843428e7
Holker Dev <nick.penkovskiy@gmail.com>
2021-12-08 18:33:33 +0100

    add find record by id

--- src/main/kotlin/eu/holker/classic/controllers/RecordController.kt
+++ src/main/kotlin/eu/holker/classic/controllers/RecordController.kt
@@ -3,6 +3,7 @@ package eu.holker.classic.controllers
 import eu.holker.classic.services.RecordService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -16,5 +17,12 @@ class RecordController(
     fun getAll(): ResponseEntity<*> {
         val records = recordService.findAllRecords()
         return ResponseEntity.ok(records)
+    }
+
+    @GetMapping("/{recordId}")
+    fun getById(@PathVariable recordId: String): ResponseEntity<*> {
+        recordService.findRecordById(recordId.toInt())
+            .fold(onSuccess = { record -> return ResponseEntity.ok(record) },
+                onFailure = { e -> return ResponseEntity.badRequest().body(e.message) })
     }
 }
--- src/main/kotlin/eu/holker/classic/services/RecordService.kt
+++ src/main/kotlin/eu/holker/classic/services/RecordService.kt
@@ -4,10 +4,20 @@ import eu.holker.classic.repositories.RecordRepository
 import eu.holker.classic.repositories.entities.dto
 import eu.holker.classic.services.dto.RecordDto
 import org.springframework.stereotype.Service
+import kotlin.Exception
 
 @Service
 class RecordService(
     private val recordRepository: RecordRepository,
 ) {
     fun findAllRecords(): List<RecordDto> = recordRepository.findAll().map { it.dto }
+
+    fun findRecordById(recordId: Int): Result<RecordDto> {
+        val recordEntity = recordRepository.findById(recordId)
+        return if (recordEntity.isEmpty) {
+            Result.failure(Exception("There is no record with provided id"))
+        } else {
+            Result.success(recordEntity.get().dto)
+        }
+    }
 }
